37134bed259ade78685afff4dacb6d9de9df47fd
Michael Debono <michael.debono@gmail.com>
2017-04-07 18:17:42 +0200

    Solved problem A

---
+++ 2016/QualificationRound/A.py
@@ -0,0 +1,17 @@
+def doit(i,n,s):
+    new_n = i*n
+    if new_n == 0:
+        return 'INSOMNIA'
+    s = s.union(set(list(str(new_n))))
+    print('{}:{}'.format(new_n,s))
+    if len(s) == 10:
+        return new_n
+    else:
+        return doit(i+1,n,s)
+
+t = int(input())
+for i in range(1, t+1):
+    n = int(input())
+    res = doit(1,n,set())
+    print('Case #{}: {}'.format(i, res))
+
